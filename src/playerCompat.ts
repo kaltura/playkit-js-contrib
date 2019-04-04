8518cbcef0a378ac6b02ca3ffe688c12cabbfc12
Eran Sakal <eran@sakalim.com>
2019-04-04 13:51:47 +0300

    wip

--- src/playerCompat.ts
+++ src/playerCompat.ts
@@ -6,18 +6,6 @@ export class PlayerCompat {
 
 	}
 
-	getServiceUrl(): string | null {
-		try {
-			// TODO find the proper api
-			// @ts-ignore
-			return __kalturaplayerdata.UIConf[Object.keys(__kalturaplayerdata.UIConf)[0]].provider.env.serviceUrl;
-		} catch (e) {
-			// do nothing
-		}
-
-		return null;
-	}
-
 	// addPortalElement(createElement: () => Component<any, any>) {
 	// 	// TODO check if it changes after media change
 	// 	const playerViewId = this.player.getView().id;
