b1abf02258c45e46b7678e93f38a5daf503cb504
Eran Sakal <eran@sakalim.com>
2019-04-07 20:34:46 +0300

    feat: add ui manager, cuepoint engine and cuepoint layout engine

---
+++ src/logger.ts
@@ -0,0 +1,29 @@
+let isEnabled = false;
+let logPrefix = "";
+export function enableLog(prefix?: string) {
+  logPrefix = prefix || "";
+  isEnabled = true;
+}
+export function log(
+  level: "debug" | "log" | "warn" | "error",
+  context: string,
+  message: string,
+  ...optionalParams: any[]
+) {
+  if (isEnabled) {
+    console[level](
+      `[${logPrefix}] [${level}]${context ? ` [${context}]` : ""} : ${message}`,
+      ...optionalParams
+    );
+  }
+}
+
+export function enableLogIfNeeded(prefix?: string) {
+  try {
+    if (document.URL.indexOf("debugKalturaPlayer") !== -1) {
+      enableLog(prefix);
+    }
+  } catch (e) {
+    // do nothing
+  }
+}
