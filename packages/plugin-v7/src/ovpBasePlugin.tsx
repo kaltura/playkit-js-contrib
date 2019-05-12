d5e590c06a88a2da427a0e354934cc4326920372
Eran Sakal <eran@sakalim.com>
2019-05-12 16:17:50 +0300

    chore(plugin-v7): add onMediaLoaded hook to base plugin and enable to logs

--- packages/plugin-v7/src/ovpBasePlugin.tsx
+++ packages/plugin-v7/src/ovpBasePlugin.tsx
@@ -2,6 +2,7 @@ import { h, render } from "preact";
 
 import { PlayerCompat } from "./playerCompat";
 import { UIManager } from "./uiManager";
+import { enableLog } from "@playkit-js/playkit-js-ovp";
 
 // TODO try to remove the 'as any'
 // @ts-ignore
@@ -16,12 +17,19 @@ export abstract class OVPBasePlugin extends (KalturaPlayer as any).core.BasePlug
 
     constructor(name: any, player: any, config: any) {
         super(name, player, config);
+
+        // TODO hook log to player log flags
+        enableLog(name);
         this._uiManager = new UIManager({ plugin: this });
     }
 
     loadMedia(): void {
         this._onAddOverlays(this._uiManager);
         this._onAddBindings(this.eventManager);
+
+        this.eventManager.listenOnce(this.player, this.player.Event.MEDIA_LOADED, () => {
+            this._onMediaLoaded();
+        });
     }
 
     public getUIManager(): UIManager {
@@ -44,6 +52,8 @@ export abstract class OVPBasePlugin extends (KalturaPlayer as any).core.BasePlug
         // TBD
         throw new Error("tbd");
     }
+
+    protected _onMediaLoaded() {}
 
     protected abstract _onAddBindings(eventManager: any): void;
     protected abstract _onAddOverlays(uiManager: UIManager): void;
