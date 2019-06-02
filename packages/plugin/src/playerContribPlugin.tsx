f6c47b2f72d288f17f7bdfb71e5da1b17b6338ae
Eran Sakal <eran@sakalim.com>
2019-06-02 11:59:33 +0300

    feat: use interfaces to expose contrib actions and guard from plugin errors
    BREAKING CHANGE:
    
    Before - using protected methods
    ```
    _onMediaLoaded();
    _onInitMembers();
    _onAddBindings(this.eventManager);
    _onAddOverlays(this._environment.uiManager);
    ```
    
    After - by implementing interfaces
    ```
    onMediaLoad(config: KalturaServerConfig): void; // interface OnMediaLoad
    onMediaUnload(): void; // interface OnMediaUnload
    onRegisterEvents(eventManager: any): void; // interface OnRegisterEvents
    onRegisterUI(uiManager: UIManager): void; // interface OnRegisterUI
    ```

--- packages/plugin/src/playerContribPlugin.tsx
+++ packages/plugin/src/playerContribPlugin.tsx
@@ -2,6 +2,45 @@ import { h, render } from "preact";
 import { OverlayManager, UIManager, UpperBarManager } from "@playkit-js-contrib/ui";
 import { EnvironmentManager } from "./environmentManager";
 
+export interface OnRegisterUI {
+	onRegisterUI(uiManager: UIManager): void;
+}
+
+function hasOnRegisterUI(plugin: any): plugin is OnRegisterUI  {
+	return 'onRegisterUI' in plugin;
+}
+
+export interface KalturaServerConfig {
+    ks: string;
+    partnerId: number;
+    serviceUrl: string;
+    entryId: string;
+}
+
+export interface OnMediaLoad {
+    onMediaLoad(config: KalturaServerConfig): void;
+}
+
+function hasOnMediaLoad(plugin: any): plugin is OnMediaLoad  {
+    return 'onMediaLoad' in plugin;
+}
+
+export interface OnMediaUnload {
+    onMediaUnload(): void;
+}
+
+function hasOnMediaUnload(plugin: any): plugin is OnMediaUnload  {
+    return 'onMediaUnload' in plugin;
+}
+
+export interface OnRegisterEvents {
+    onRegisterEvents(eventManager: any): void;
+}
+
+function hasOnRegisterEvents(plugin: any): plugin is OnRegisterEvents  {
+    return 'onRegisterEvents' in plugin;
+}
+
 // TODO try to remove the 'as any'
 // @ts-ignore
 export abstract class PlayerContribPlugin extends (KalturaPlayer as any).core.BasePlugin {
@@ -19,24 +58,49 @@ export abstract class PlayerContribPlugin extends (KalturaPlayer as any).core.Ba
     });
 
     loadMedia(): void {
-        this._onAddBindings(this.eventManager);
-        this._onAddOverlays(this._environment.uiManager);
+        if (hasOnRegisterUI(this)) {
+            try {
+                this.onRegisterUI(this._environment.uiManager)
+            } catch(e) {
+                console.log(`failed to register contrib ui items for plugin`, { error: e.message });
+            }
+        }
 
-        this.eventManager.listenOnce(this.player, this.player.Event.MEDIA_LOADED, () => {
-            this._onMediaLoaded();
-        });
+    	if (hasOnRegisterEvents(this)) {
+            try {
+                this.onRegisterEvents(this.eventManager);
+            } catch (e) {
+                console.log(`failed to register to kaltura player events`, {error: e.message});
+            }
+        }
+
+        if (hasOnMediaLoad(this)) {
+            this.eventManager.listenOnce(this.player, this.player.Event.MEDIA_LOAD, () => {
+                try {
+                    const config = this.getKalturaServerConfig();
+                    this.onMediaLoad(config);
+                } catch (e) {
+                    console.log(`failure during media load `, {error: e.message});
+                }
+            });
+        }
     }
 
     public destroy() {
-        this.eventManager.removeAll();
+        this.reset();
         this.eventManager.destroy();
-        this._onResetState();
     }
 
     public reset() {
         this.eventManager.removeAll();
         this._environment.uiManager.reset();
-        this._onInitMembers();
+        if (hasOnMediaUnload(this)) {
+            try {
+                this.onMediaUnload();
+            } catch (e) {
+                console.log(`failure during media unload`, {error: e.message});
+            }
+        }
     }
 
     protected _sendAnalytics() {
@@ -44,24 +108,12 @@ export abstract class PlayerContribPlugin extends (KalturaPlayer as any).core.Ba
         throw new Error("tbd");
     }
 
-    protected _onMediaLoaded() {}
-    protected abstract _onAddOverlays(uiManager: UIManager): void;
-    protected abstract _onAddBindings(eventManager: any): void;
-    protected abstract _onInitMembers(): void;
-
-    getServiceUrl(): string {
-        return this.player.config.provider.env.serviceUrl;
-    }
-
-    getEntryId(): string {
-        return this.player.config.sources.id;
-    }
-
-    getPartnerId(): number {
-        return this.player.config.session.partnerId;
-    }
-
-    getKS(): string {
-        return this.player.config.session.ks;
+    getKalturaServerConfig(): KalturaServerConfig {
+        return {
+            ks: this.player.config.session.ks,
+            serviceUrl: this.player.config.provider.env.serviceUrl,
+            partnerId: this.player.config.session.partnerId,
+            entryId: this.player.config.sources.id
+        }
     }
 }
