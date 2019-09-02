e60ec6edc39bfa01b53a7cacf4ec1293fe580316
ainvoner <shai.ainvoner@kaltura.com>
2019-09-02 16:08:18 +0300

    WEBC-1450 - logger refactor and interface method name change

--- packages/ui/src/components/announcement-container/announcementContainer.tsx
+++ packages/ui/src/components/announcement-container/announcementContainer.tsx
@@ -3,42 +3,35 @@ import { ContribLogger, getContribLogger } from "@playkit-js-contrib/common";
 import * as styles from "./_announcementContainer.scss";
 
 export interface AnnouncementContainerProps {
-    handleCloseEvent: (event: any) => void;
+    onClose: (event: any) => void;
 }
 
 export class AnnouncementContainer extends Component<AnnouncementContainerProps> {
-    private _logger: ContribLogger | null = null;
+    private _logger: ContribLogger = getContribLogger({
+        module: "contrib-ui",
+        class: "AnnouncementContainer"
+    });
 
     componentDidMount(): void {
-        this._logger = getContribLogger({
-            module: "contrib-ui",
-            class: "AnnouncementContainer"
-        });
         this._logger.info(`mount component`, {
             method: "componentDidMount"
         });
     }
 
     componentWillUnmount(): void {
-        if (!this._logger) {
-            return;
-        }
-
         this._logger.info(`unmount component`, {
             method: "componentWillUnmount"
         });
     }
 
     render(props: AnnouncementContainerProps) {
-        if (this._logger) {
-            this._logger.trace(`render component`, {
-                method: "render"
-            });
-        }
+        this._logger.trace(`render component`, {
+            method: "render"
+        });
         return (
             <div className={styles.announcementContainerRoot}>
                 <div className={styles.announcementContainer}>
-                    <button className={styles.closeButton} onClick={props.handleCloseEvent} />
+                    <button className={styles.closeButton} onClick={props.onClose} />
                     {this.props.children}
                 </div>
             </div>
