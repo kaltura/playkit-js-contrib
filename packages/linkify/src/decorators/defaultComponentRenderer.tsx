40464fabbb655cdae90113d7b812dbc49c77e586
Adi Machmali <adimachmali@gmail.com>
2019-10-07 11:21:25 +0300

    fix(Linkify): open link in a new tab

--- packages/linkify/src/decorators/defaultComponentRenderer.tsx
+++ packages/linkify/src/decorators/defaultComponentRenderer.tsx
@@ -2,7 +2,7 @@ import { ComponentChild, h } from "preact";
 
 export default (href: string, text: string, key: number): ComponentChild => {
     return (
-        <a href={href} key={key}>
+        <a href={href} key={key} target={"_blank"}>
             {text}
         </a>
     );
