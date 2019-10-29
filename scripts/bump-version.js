56c81b9d13f0fd079f00625f3a805aafcab257a8
Eran Sakal <eran@sakalim.com>
2019-10-30 01:40:50 +0200

    improve version script

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -3,28 +3,28 @@
 const chalk = require('chalk');
 const path = require('path');
 const util = require('util');
-const spawn = util.promisify(require('child_process').spawn);
+const spawnSync = util.promisify(require('child_process').spawnSync);
 
 const rootPath = path.resolve(__dirname,'../');
 const lernaPath = 'node_modules/.bin/lerna';
 const args = process.argv.splice(2);
 
-(async function() {
-  await spawn(lernaPath, ['version', '--no-push', '--no-git-tag-version', ...args ], {cwd: rootPath, stdio: 'inherit'});
+(function() {
+  spawnSync(lernaPath, ['version', '--no-push', '--no-git-tag-version', ...args ], {cwd: rootPath, stdio: 'inherit'});
 
   const {version: packageVersion} = require('../lerna.json');
   console.log(chalk`
-    {green bump contrib libraries to version {bold ${packageVersion}}} and update CHANGELOG.md files.
+    {green Successfully updated contrib libraries versions to {bold ${packageVersion}} and updated relevant CHANGELOG.md files.}
     
+    Please review changes before continuing with the publish.
+      
     To abort changes run:
     {bold git reset --hard}
     
     To commit changes to github run:
     {bold git commit -am "chore(release): publish ${packageVersion}}"
     {bold git tag v${packageVersion}}
-    {bold git push --follow-tags}
-   
-    
+    {bold git push --follow-tags}  
   `);
 })();
 
