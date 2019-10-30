dca27c5d9cae8df4f0c560f0945095a147548c58
Eitan Avgil <eitan.avgil@kaltura.com>
2019-10-30 13:41:07 +0200

    chore(common): move IEngineDecorator and IEngineDecoratorProvider interfaces to contrib

--- packages/common/global-types/index.d.ts
+++ packages/common/global-types/index.d.ts
@@ -3,6 +3,7 @@
 /// <reference path="./kaltura-player/player-config.d.ts" />
 /// <reference path="./kaltura-player/base-plugin.d.ts" />
 /// <reference path="./kaltura-player/base-middleware.d.ts" />
+/// <reference path="./kaltura-player/engine-decorator.d.ts" />
 /// <reference path="./kaltura-player/logger.d.ts" />
 /// <reference path="./kaltura-player/player.d.ts" />
 /// <reference path="./kaltura-player/kaltura-player.d.ts" />
