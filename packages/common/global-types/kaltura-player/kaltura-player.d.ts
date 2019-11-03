7c36d18db3ead93a6eb119ea0ad4ba0023400595
Eran Sakal <eran@sakalim.com>
2019-11-03 11:30:15 +0200

    chore: update kaltura player declaration

--- packages/common/global-types/kaltura-player/kaltura-player.d.ts
+++ packages/common/global-types/kaltura-player/kaltura-player.d.ts
@@ -1,33 +1,38 @@
-import * as ContribPreact from "preact";
+import * as ContribPreact from 'preact';
 
 declare global {
-    const KalturaPlayer: {
-        ui: {
-            EventType: Record<string, string>;
-            redux: {
-                connect: (...args: any) => any;
-            };
-            reducers: Record<string, { actions: Record<string, unknown>[] }>;
-            preact: typeof ContribPreact;
-            utils: {
-                getLogger: (name: string) => KalturaPlayerTypes.Logger;
-                bindActions(actions: Record<string, unknown>[]): (...args: any) => void;
-            };
-            components: {};
-        };
-        core: {
-            registerPlugin(name: string, component: any): void;
-            BasePlugin: {
-                new (...args: any[]): KalturaPlayerTypes.BasePlugin;
-            };
-            BaseMiddleware: {
-                new (): KalturaPlayerTypes.BaseMiddleware;
-            };
-            utils: {
-                Object: {
-                    mergeDeep(target: Record<string, any>, ...sources: Record<string, any>[]);
-                };
-            };
-        };
+  const KalturaPlayer: {
+    ui: {
+      EventType: Record<string, string>;
+      redux: {
+        connect: (...args: any) => any;
+      };
+      reducers: Record<string, {actions: Record<string, unknown>[]}>;
+      preact: typeof ContribPreact;
+      utils: {
+        getLogger: (name: string) => KalturaPlayerTypes.Logger;
+        bindActions(actions: Record<string, unknown>[]): (...args: any) => void;
+      };
+      components: {
+        withPlayer: any;
+      };
     };
+    core: {
+      registerPlugin(name: string, component: any): void;
+      BasePlugin: {
+        new (...args: any[]): KalturaPlayerTypes.BasePlugin;
+      };
+      BaseMiddleware: {
+        new (): KalturaPlayerTypes.BaseMiddleware;
+      };
+      utils: {
+        Object: {
+          mergeDeep(
+            target: Record<string, any>,
+            ...sources: Record<string, any>[]
+          );
+        };
+      };
+    };
+  };
 }
