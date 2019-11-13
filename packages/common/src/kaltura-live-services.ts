b21e195e04fbe03622d999043d5ff06338734c82
Adi Machmali <adimachmali@gmail.com>
2019-11-13 14:04:48 +0200

    fix(common): fix how we creat uuid for anonymous user (webc-1694)

--- packages/common/src/kaltura-live-services.ts
+++ packages/common/src/kaltura-live-services.ts
@@ -1,10 +1,12 @@
 import {UUID} from './uuid';
 
+const HashSeparatorText = 'HashSeparator';
+
 export class KalturaLiveServices {
-  public static getAnonymousUserId(): string {
+  public static getAnonymousUserId(userId: string): string {
     if (typeof Storage === 'undefined') {
       // No web storage Support, just generate and return a anonymous user id
-      return KalturaLiveServices._generateAnonymousUserId();
+      return KalturaLiveServices._generateAnonymousUserId(userId);
     }
 
     // Web storage is supported
@@ -12,14 +14,37 @@ export class KalturaLiveServices {
     anonymousUserId = localStorage.getItem('anonymousUserId');
 
     if (!anonymousUserId) {
-      anonymousUserId = KalturaLiveServices._generateAnonymousUserId();
+      anonymousUserId = KalturaLiveServices._generateAnonymousUserId(userId);
       localStorage.setItem('anonymousUserId', anonymousUserId);
     }
 
     return anonymousUserId;
   }
 
-  private static _generateAnonymousUserId() {
-    return `##-${UUID.uuidV1()}-##`;
+  private static _generateAnonymousUserId(userId: string) {
+    const numFromUuid = KalturaLiveServices._getNumberStringUuid();
+
+    return `##${userId}${HashSeparatorText}${numFromUuid}##`;
+  }
+
+  private static _getNumberStringUuid(): string {
+    const uuid = UUID.uuidV1();
+    let numStr = '';
+
+    for (let i = 0; i < uuid.length; i++) {
+      const char = uuid.charAt(i);
+
+      if (KalturaLiveServices.isNan(+char)) {
+        numStr = numStr + char.charCodeAt(0);
+      } else {
+        numStr = numStr + char;
+      }
+    }
+
+    return numStr;
+  }
+
+  private static isNan(value) {
+    return value !== null && (value != value || +value != value);
   }
 }
