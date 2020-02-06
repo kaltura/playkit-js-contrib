f33c6961085b03e99b4ca5dc4a188e83a9b79b47
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2020-02-06 18:24:26 +0200

    chore(ui): fix PR comments (REACH2-786)

--- packages/ui/src/local-content-utils.ts
+++ packages/ui/src/local-content-utils.ts
@@ -4,24 +4,23 @@ export function downloadContent(content: string, name: string): void {
   if (window.navigator.msSaveBlob) {
     // IE
     window.navigator.msSaveOrOpenBlob(blob, name);
-  } else if (navigator.userAgent.search('Firefox') !== -1) {
+    return;
+  }
+  if (navigator.userAgent.search('Firefox') !== -1) {
     // Firefox
     anchor.style.display = 'none';
     anchor.setAttribute(
       'href',
       'data:text/plain;charset=utf-8,' + encodeURIComponent(content)
     );
-    anchor.setAttribute('target', '_blank');
-    anchor.setAttribute('download', name);
-    anchor.click();
-    anchor.remove();
   } else {
     // Chrome
     anchor.setAttribute('href', URL.createObjectURL(blob));
-    anchor.setAttribute('target', '_blank');
-    anchor.setAttribute('download', name);
-    anchor.click();
   }
+  anchor.setAttribute('target', '_blank');
+  anchor.setAttribute('download', name);
+  anchor.click();
+  anchor.remove();
 }
 
 export function printContent(content: string): void {
