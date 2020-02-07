a829f478c6ffc0cb93c52e108604256e5ae3bd07
Serhii <serhii.marchenko@Kaltura.com>
2020-02-07 12:09:28 +0200

    Reach2 786 ie edge download issue (#182)
    * chore(ui): fix issue with downloading in IE and Edge (REACH2-786)
    
    * chore(ui): set content type (REACH2-786)
    
    * chore(ui): fix PR comments (REACH2-786)

--- packages/ui/src/local-content-utils.ts
+++ packages/ui/src/local-content-utils.ts
@@ -1,11 +1,26 @@
 export function downloadContent(content: string, name: string): void {
-  const link = document.createElement('a');
-  link.setAttribute(
-    'href',
-    'data:text/plain;charset=utf-8,' + encodeURIComponent(content)
-  );
-  link.setAttribute('download', name);
-  link.click();
+  const blob = new Blob([content], {type: 'text/plain;charset=utf-8;'});
+  const anchor = document.createElement('a');
+  if (window.navigator.msSaveBlob) {
+    // IE
+    window.navigator.msSaveOrOpenBlob(blob, name);
+    return;
+  }
+  if (navigator.userAgent.search('Firefox') !== -1) {
+    // Firefox
+    anchor.style.display = 'none';
+    anchor.setAttribute(
+      'href',
+      'data:text/plain;charset=utf-8,' + encodeURIComponent(content)
+    );
+  } else {
+    // Chrome
+    anchor.setAttribute('href', URL.createObjectURL(blob));
+  }
+  anchor.setAttribute('target', '_blank');
+  anchor.setAttribute('download', name);
+  anchor.click();
+  anchor.remove();
 }
 
 export function printContent(content: string): void {
