7c2bbbc1bd4a1258179f9d1f2fb65947bfdc6917
semarche-kaltura <serhii.marchenko@Kaltura.com>
2021-07-05 15:52:21 +0300

    fix(fev-964): address PR comments

--- packages/ui/src/components/managed-component/managed-component.tsx
+++ packages/ui/src/components/managed-component/managed-component.tsx
@@ -40,31 +40,12 @@ export class ManagedComponent extends Component<
     });
   }
 
-  shouldComponentUpdate(
-    prevProps: Readonly<ManagedComponentProps>,
-    prevState: Readonly<ManagedComponentState>
-  ): boolean {
-    const {toggler} = this.state;
-    const {
-      isShown,
-      renderChildren,
-      label,
-      fillContainer,
-      updateOnPlayerSizeChanged,
-      playerSize,
-    } = this.props;
-    if (
-      prevState.toggler !== toggler ||
-      prevProps.isShown !== isShown ||
-      prevProps.renderChildren !== renderChildren ||
-      prevProps.label !== label ||
-      prevProps.fillContainer !== fillContainer ||
-      prevProps.updateOnPlayerSizeChanged !== updateOnPlayerSizeChanged ||
-      (updateOnPlayerSizeChanged && prevProps.playerSize !== playerSize)
-    ) {
-      return true;
-    }
-    return false;
+  shouldComponentUpdate(prevProps: Readonly<ManagedComponentProps>): boolean {
+    const {updateOnPlayerSizeChanged, playerSize} = this.props;
+    return (
+      (updateOnPlayerSizeChanged && prevProps.playerSize !== playerSize) ||
+      prevProps.playerSize === playerSize
+    );
   }
 
   componentDidMount(): void {
